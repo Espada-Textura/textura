a10d6a3c7e5a4a9dbe4dd69003169ca316609c44
NirPisatto <76983137+NirPisatto@users.noreply.github.com>
2022-01-06 13:54:50 +0700

    [frontend][feature] - Added react environment

---
+++ frontend/textura/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Textura</h1>
+        <p>&#169;2022 by Espada</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
