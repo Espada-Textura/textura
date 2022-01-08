7699e8c6be6ac5142ddd58ae277e4bae21883e0b
ChamroeunProMaxQ <stevenkakoreal@gmail.com>
2022-01-08 11:46:50 +0700

    [front page][update] - add Brave dragon link

--- frontend/textura/src/App.js
+++ frontend/textura/src/App.js
@@ -7,7 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Textura</h1>
-        <p>&#169;2022 by Espada</p>
+        <p>&#169;2022 by Espada <a href="https://bravedragon.rf.gd/?i=1" > - Brave Dragon Co,Ltd</a>
+             </p>
+
       </header>
     </div>
   );
