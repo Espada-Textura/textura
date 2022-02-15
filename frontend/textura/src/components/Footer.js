269eb6941cfb2d0028e1d3aaba8fc4e9c5b4145d
Pisatto <misapisatto@gmail.com>
2022-02-15 16:45:01 +0700

    [render][footer][deploy]

--- frontend/textura/src/components/Footer.js
+++ frontend/textura/src/components/Footer.js
@@ -1,5 +1,25 @@
+import TexturaLogo from '@assets/logo.png'
+
+import '@styles/components/Footer.css'
+
 function Footer() {
-    return <div>Footer</div>
+    return (
+        <div>
+            <div className="footer-main-layout col-12">
+                <div className="col-2 d-flex">
+                    <img
+                        src={TexturaLogo}
+                        alt="Textura Logo"
+                        width="100px"
+                        height="100px"
+                    ></img>
+
+                    <div className="footer-logo-text">Textura</div>
+                </div>
+                <div className="col-10"></div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
