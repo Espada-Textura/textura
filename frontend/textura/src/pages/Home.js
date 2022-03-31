76806d35902eee3ae781ea5e69a510cb8dc3d25d
Pisatto <misapisatto@gmail.com>
2022-03-31 22:14:56 +0700

    Refactor home page

--- frontend/textura/src/pages/Home.js
+++ frontend/textura/src/pages/Home.js
@@ -4,6 +4,17 @@ import HomepagePic from '@images/HomePage_Pic.png'
 import EspadaLogo from '@images/Espada.svg'
 import UploadForm from '@components/UploadForm'
 import ArtistProfile from '@images/ado.jpg'
+import Button from '@mui/material/Button'
+import ImageList from '@mui/material/ImageList'
+import ImageListItem from '@mui/material/ImageListItem'
+import Masonry from '@mui/lab/Masonry'
+import ArtsList from '@components/ArtsList'
+
+// redux
+import { useDispatch, useSelector } from 'react-redux'
+import { getArts } from '@redux/art/operations'
+import { allArts } from '@redux/art/selectors'
+import { useEffect, useState } from 'react'
 
 import {
     useNavigate,
@@ -11,8 +22,6 @@ import {
     useMatch,
     useLocation,
 } from 'react-router-dom'
-
-import { useState } from 'react'
 
 function Home() {
     const [show, setShow] = useState(false)
@@ -24,140 +33,116 @@ function Home() {
     }
     const handleClose = () => setShow(false)
     const handleShow = () => setShow(true)
+    // data
+    let [arts, setArts] = useState([])
+
+    // redux
+    const dispatch = useDispatch()
+    const selector = useSelector((state) => state)
+    useEffect(() => {
+        dispatch(getArts())
+    }, [])
+    useEffect(() => {
+        setArts(allArts(selector))
+    }, [allArts(selector)])
+    const itemData = [
+        {
+            img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
+            title: 'Bed',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+            title: 'Kitchen',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+            title: 'Sink',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+            title: 'Books',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+            title: 'Chairs',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+            title: 'Candle',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+            title: 'Laptop',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+            title: 'Doors',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+            title: 'Coffee',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+            title: 'Storage',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+            title: 'Coffee table',
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+            title: 'Blinds',
+        },
+    ]
     return (
         <div className="home-main-layout">
             <section className="pb-5">
-                <div className="col-12">
-                    <img
+                <div className=" col-12 home-background">
+                    {/* <img
                         className="homwpage-pic col-12"
                         src={HomepagePic}
                         alt="Homepage Picture"
-                    ></img>
-                    <div className="flex col-5">
+                    ></img> */}
+                    <div className="flex col-12 col-md-6 p-5 home-main-info-layout">
                         <div className="home-info">
-                            <div className="home-info-title">Together Grow</div>
-                            <div className="home-info-text">
+                            <h1 className="home-info-title pb-3">
+                                Together Grow
+                            </h1>
+                            <h4 className="">
                                 Textura is a place, likely a community, for our
                                 Cambodian and Artists to share their arts, earn
                                 recognition, express feeling, and emotion
                                 through art, and more importantly is to improve
                                 the artist community in Cambodia.
-                            </div>
+                            </h4>
                         </div>
-                        <div>
-                            <button
-                                className="home-btn-create-post"
+                        <div className="col-12 pt-3 pb-3">
+                            <Button
+                                className="home-btn-create-post col-12"
                                 onClick={handleShow}
                             >
                                 <FiPlusCircle className="home-icon" />
                                 CREATE POST
-                            </button>
+                            </Button>
                         </div>
                         <div>
-                            <button
-                                className="home-btn-sign-up"
+                            <Button
+                                className="home-btn-sign-up col-12"
                                 onClick={(e) => {
                                     PushTo(e, 'register')
                                 }}
                             >
                                 <FiLogIn className="home-icon" />
                                 SIGN UP
-                            </button>
+                            </Button>
                         </div>
                     </div>
                 </div>
             </section>
-            <section className="pt-5 pb-5">
-                <div className='mx-4'>
-                    <div className="leaderboard-title">Trending Post</div>
-                    <div className="d-flex justify-content-between">
-                        <div className="">
-                            {/*Post Ranking #1*/}
-                            <img
-                                className="ranked-art img-fluid img-thumbnail"
-                                src={EspadaLogo}
-                                alt="Espada Logo"
-                            ></img>
-                        </div>
-                        <div className="">
-                            {/*Post Ranking #2*/}
-                            <img
-                                className="ranked-art img-fluid img-thumbnail"
-                                src={EspadaLogo}
-                                alt="Espada Logo"
-                            ></img>
-                        </div>
-                        <div className="">
-                            {/*Post Ranking #3*/}
-                            <img
-                                className="ranked-art img-fluid img-thumbnail"
-                                src={EspadaLogo}
-                                alt="Espada Logo"
-                            ></img>
-                        </div>
-                        <div className="">
-                            {/*Post Ranking #4*/}
-                            <img
-                                className="ranked-art img-fluid img-thumbnail"
-                                src={EspadaLogo}
-                                alt="Espada Logo"
-                            ></img>
-                        </div>
-                        <div className="">
-                            {/*Post Ranking #5*/}
-                            <img
-                                className="ranked-art img-fluid img-thumbnail"
-                                src={EspadaLogo}
-                                alt="Espada Logo"
-                            ></img>
-                        </div>
-                    </div>
-                </div>
-                <div className="mx-4">
-                    <div className="leaderboard-title">Popular Arttists</div>
-                    <div className="d-flex justify-content-between">
-                        <div className="">
-                            {/*Profile Ranking #1 Artist*/}
-                            <img
-                                className="artist-profile img-fluid img-thumbnail"
-                                src={ArtistProfile}
-                                alt="Profile Picture"
-                            ></img>
-                        </div>
-                        <div className="">
-                            {/*Profile Ranking #2 Artist*/}
-                            <img
-                                className="artist-profile img-fluid img-thumbnail"
-                                src={ArtistProfile}
-                                alt="Profile Picture"
-                            ></img>
-                        </div>
-                        <div className="">
-                            {/*Profile Ranking #3 Artist*/}
-                            <img
-                                className="artist-profile img-fluid img-thumbnail"
-                                src={ArtistProfile}
-                                alt="Profile Picture"
-                            ></img>
-                        </div>
-                        <div className="">
-                            {/*Profile Ranking #4 Artist*/}
-                            <img
-                                className="artist-profile img-fluid img-thumbnail"
-                                src={ArtistProfile}
-                                alt="Profile Picture"
-                            ></img>
-                        </div>
-                        <div className="">
-                            {/*Profile Ranking #5 Artist*/}
-                            <img
-                                className="artist-profile img-fluid img-thumbnail"
-                                src={ArtistProfile}
-                                alt="Profile Picture"
-                            ></img>
-                        </div>
-                    </div>
-                </div>
+            <section className="">
+                <div className="leaderboard-title p-4">New arts</div>
+                <ArtsList arts={arts}></ArtsList>
             </section>
         </div>
     )
