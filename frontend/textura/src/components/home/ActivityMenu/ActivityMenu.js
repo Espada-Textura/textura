001a192234a4b721833bb8aed68f3639879b0f96
Pisatto <misapisatto@gmail.com>
2022-05-21 10:53:58 +0700

    Refactoring home page

--- frontend/textura/src/components/home/ActivityMenu/ActivityMenu.js
+++ frontend/textura/src/components/home/ActivityMenu/ActivityMenu.js
@@ -1,20 +1,36 @@
 import './ActivityMenu.css'
+import Box from '@mui/material/Box'
+import ArtistProfile from '@images/ado.jpg'
 
 function ActivityMenu(props) {
+    const cardStyles = {
+        backgroundImage: `url('${
+            props.image
+                ? props.image
+                : 'https://drive.google.com/uc?export=view&id=10ZikNlF86q_oB2Jy_c97UfDL8mv_rlo8'
+        }')`,
+    }
     return (
-        <div className={'container active'}>
-            <div className="card">
+        <Box
+            className={'container'}
+            sx={{
+                width: props.width ? props.width : 350,
+            }}
+        >
+            <div className="card" style={cardStyles}>
                 <div className={`background`} />
                 <div className={`title-background`}>
-                    <h3 className="title">ARTs</h3>
+                    <h3 className="title">
+                        {props.title ? props.title : 'Unknown'}
+                    </h3>
                     <p className="title-detail">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-                        ut aliquam, purus sit amet luctus venenatis, lectus
-                        magna fringilla urna.
+                        {props.content
+                            ? props.content
+                            : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna.'}
                     </p>
                 </div>
             </div>
-        </div>
+        </Box>
     )
 }
 
--- frontend/textura/src/pages/Home.js
+++ frontend/textura/src/pages/Home.js
@@ -5,6 +5,7 @@ import {
     useMatch,
     useLocation,
 } from 'react-router-dom'
+import { useTransition, useTrail, animated, config } from 'react-spring'
 
 // Style
 import '@styles/pages/Home.css'
@@ -14,26 +15,56 @@ import { generateVisibleGoogleDriveImageURL } from '@helper'
 
 // Components
 import ActivityMenu from '@components/home/ActivityMenu'
+import Grow from '@mui/material/Grow'
 
 // redux
 import { useDispatch, useSelector } from 'react-redux'
 import { getArts } from '@redux/art/operations'
 import { allArts } from '@redux/art/selectors'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
-    // Animation
-    const [state, toggle] = useState(false)
+    // menus
+    const allMenu = [
+        {
+            title: 'Now Trending ART',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna.',
+            width: 350,
+            image: '10ZikNlF86q_oB2Jy_c97UfDL8mv_rlo8',
+        },
+        {
+            title: 'Now Trending ART',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna.',
+            width: 350,
+            image: '1LxfNe067obyHGxcNHinX6NljPPw4QHR4',
+        },
+        {
+            title: 'ARTs',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna.',
+            width: 600,
+            image: '1tZmhWvGXsY-JrZSwGjUYXS1Rs0DtYvNj',
+        },
+        {
+            title: 'Setting',
+            content:
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna.',
+            width: 400,
+            image: '1xPqmBYPBc9H5vxFpn4kfOOfRtRsgbck1',
+        },
+    ]
 
-    const [show, setShow] = useState(false)
+    // Animation
+    const [mounted, toggleMounted] = useState(false)
+
     const navigate = useNavigate()
     let location = useLocation()
     const PushTo = function (e, pathname = '') {
         e.preventDefault()
         if (location.pathname !== '/' + pathname) navigate(pathname)
     }
-    const handleClose = () => setShow(false)
-    const handleShow = () => setShow(true)
     // data
     let [arts, setArts] = useState([])
 
@@ -41,18 +72,22 @@ function Home() {
     const dispatch = useDispatch()
     const selector = useSelector((state) => state)
     useEffect(() => {
-        dispatch(getArts())
+        // dispatch(getArts())
+        // toggleMounted(!mounted)
     }, [])
     useEffect(() => {
-        setArts(allArts(selector))
+        // setArts(allArts(selector))
     }, [allArts(selector)])
+
     return (
         <div className="home-main-layout">
             <div>
                 <img
                     className="background-img"
-                    rel="preload"
                     src={generateVisibleGoogleDriveImageURL()}
+                    onLoad={() => {
+                        toggleMounted(!mounted)
+                    }}
                 ></img>
             </div>
             <div className="home-main-content-layout">
@@ -64,11 +99,29 @@ function Home() {
                 </div>
 
                 <div className="home-main-content-layout-activity-items p-5">
-                    <ActivityMenu />
-                    <ActivityMenu />
-                    <ActivityMenu />
-                    <ActivityMenu />
-                    <ActivityMenu />
+                    {allMenu.map((element, index) => {
+                        return (
+                            <Grow
+                                key={index}
+                                in={mounted}
+                                style={{ transformOrigin: '0 0 0' }}
+                                {...(mounted
+                                    ? { timeout: 500 * (index + 1) }
+                                    : {})}
+                            >
+                                <div>
+                                    <ActivityMenu
+                                        title={element.title}
+                                        content={element.content}
+                                        image={generateVisibleGoogleDriveImageURL(
+                                            element.image
+                                        )}
+                                        width={element.width}
+                                    />
+                                </div>
+                            </Grow>
+                        )
+                    })}
                 </div>
             </div>
         </div>
