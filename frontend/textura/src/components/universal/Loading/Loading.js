cec89ffa002e9e16e7f646ec22cfb9e9f489a55a
Pisatto <misapisatto@gmail.com>
2022-05-26 21:56:19 +0700

    Render loading animation

---
+++ frontend/textura/src/components/universal/Loading/Loading.js
@@ -0,0 +1,23 @@
+import './Loading.css'
+
+function Loading() {
+    return (
+        <div>
+            <div className="loadingio-spinner-blocks-fhze938aqwq">
+                <div className="ldio-mvxohi746r">
+                    <div className="div1"></div>
+                    <div className="div2"></div>
+                    <div className="div3"></div>
+                    <div className="div4"></div>
+                    <div className="div5"></div>
+                    <div className="div6"></div>
+                    <div className="div7"></div>
+                    <div className="div8"></div>
+                    <div className="div9"></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
